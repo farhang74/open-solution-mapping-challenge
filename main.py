a6815fc04f0eb99ef7b3651a74a2ad386dddda30
Farhang <zarefarhang@gmail.com>
2021-06-25 21:56:48 +0430

    fix: command not found

--- main.py
+++ main.py
@@ -9,20 +9,20 @@ def main():
     pass
 
 
-@main.command()
+@main.command('prepare_masks')
 @click.option('-d', '--dev_mode', help='if true only a small sample of data will be used', is_flag=True, required=False)
 def prepare_masks(dev_mode):
     pipeline_manager.prepare_masks(dev_mode)
 
 
-@main.command()
+@main.command('prepare_metadata')
 @click.option('-tr', '--train_data', help='calculate for train data', is_flag=True, required=False)
 @click.option('-val', '--valid_data', help='calculate for validation data', is_flag=True, required=False)
 def prepare_metadata(train_data, valid_data):
     pipeline_manager.prepare_metadata(train_data, valid_data)
 
 
-@main.command()
+@main.command('train')
 @click.option('-p', '--pipeline_name', help='pipeline to be trained', required=True)
 @click.option('-d', '--dev_mode', help='if true only a small sample of data will be used', is_flag=True, required=False)
 def train(pipeline_name, dev_mode):
@@ -31,7 +31,7 @@ def train(pipeline_name, dev_mode):
     pipeline_manager.finish_experiment()
 
 
-@main.command()
+@main.command('evaluate')
 @click.option('-p', '--pipeline_name', help='pipeline to be trained', required=True)
 @click.option('-d', '--dev_mode', help='if true only a small sample of data will be used', is_flag=True, required=False)
 @click.option('-c', '--chunk_size', help='size of the chunks to run evaluation on', type=int, default=None,
@@ -42,7 +42,7 @@ def evaluate(pipeline_name, dev_mode, chunk_size):
     pipeline_manager.finish_experiment()
 
 
-@main.command()
+@main.command('predict_on_dir')
 @click.option('-p', '--pipeline_name', help='pipeline to be trained', required=True)
 @click.option('-d', '--dir_path', help='directory with images to score', required=True)
 @click.option('-r', '--prediction_path', help='path to the prediction .json file', required=True)
